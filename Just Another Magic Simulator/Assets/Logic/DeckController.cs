64f5f047bb066f7b37dbb1b1f7b2b1026738a105
woshikie <woshikie@gmail.com>
2016-10-15 21:39:51 +0800

    Developments -Started development on API to get Deck from github

---
+++ Just Another Magic Simulator/Assets/Logic/DeckController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DeckController : MonoBehaviour {
+    [SerializeField]
+    List<string> mainboard = new List<string>();
+    [SerializeField]
+    List<string> sideboard = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+	    //Get Deck list from API
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
